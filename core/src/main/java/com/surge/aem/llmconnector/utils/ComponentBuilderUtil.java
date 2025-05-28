3e065c01ae82aac01bf16dae5d14ba742d9d0252
surgeproducts <corp@surgesoftware.co.in>
2025-05-28 15:07:52 +0530

    Add ComponentBuilderUtil and llm-prompt.json sample

---
+++ core/src/main/java/com/surge/aem/llmconnector/utils/ComponentBuilderUtil.java
@@ -0,0 +1,53 @@
+package com.surge.aem.llmconnector.utils;
+
+import org.apache.sling.api.resource.ModifiableValueMap;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceUtil;
+import org.apache.sling.api.resource.PersistenceException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ComponentBuilderUtil {
+
+    public static void createComponent(ResourceResolver resolver, JSONObject compDef) throws PersistenceException {
+        String name = compDef.getString("componentName");
+        JSONArray fields = compDef.getJSONArray("fields");
+
+        String basePath = "/apps/llmconnector/components/" + name;
+
+        // Create component folder
+        Resource compFolder = ResourceUtil.getOrCreateResource(resolver, basePath, "cq:Component", "nt:unstructured", true);
+
+        // Set component metadata
+        ModifiableValueMap vm = compFolder.adaptTo(ModifiableValueMap.class);
+        if (vm != null) {
+            vm.put("jcr:title", name);
+            vm.put("componentGroup", "LLM Generated");
+        }
+
+        // Create dialog root
+        Resource dialog = ResourceUtil.getOrCreateResource(resolver, basePath + "/cq:dialog", "nt:unstructured", null, true);
+        Resource content = ResourceUtil.getOrCreateResource(resolver, dialog.getPath() + "/content", "nt:unstructured", null, true);
+
+        // Create fields inside dialog content
+        for (int i = 0; i < fields.length(); i++) {
+            JSONObject field = fields.getJSONObject(i);
+            String fieldName = field.getString("name");
+            String type = field.getString("type");
+
+            Map<String, Object> props = new HashMap<>();
+            props.put("jcr:primaryType", "nt:unstructured");
+            props.put("sling:resourceType", "granite/ui/components/coral/foundation/form/" + type);
+            props.put("name", "./" + fieldName);
+            props.put("fieldLabel", fieldName);
+
+            ResourceUtil.getOrCreateResource(resolver, content.getPath() + "/" + fieldName, props, "nt:unstructured", true);
+        }
+
+        resolver.commit();
+    }
+}
